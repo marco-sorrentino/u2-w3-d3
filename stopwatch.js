9072fae5ec7492c157b7732cc6706832996d4f39
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-01-25 16:58:31 +0100

    fixed

--- stopwatch.js
+++ stopwatch.js
@@ -1,51 +1,13 @@
-let seconds = parseInt(sessionStorage.getItem("lastCounterValue1")) || 00;
-let tens = parseInt(sessionStorage.getItem("lastCounterValue")) || 00;
-let appendTens = document.getElementById("tens");
-let appendSeconds = document.getElementById("seconds");
-let buttonStart = document.getElementById("start");
-let buttonStop = document.getElementById("stop");
-let buttonReset = document.getElementById("reset");
-let Interval;
+const displayTime = document.getElementById("displayTime");
+const start = document.getElementById("start");
+let count = sessionStorage.getItem("counterValue") || 0;
 
-buttonStart.addEventListener("click", () => {
-  clearInterval(Interval);
-  Interval = setInterval(startTimer, 10);
-});
-
-buttonStop.addEventListener("click", () => {
-  clearInterval(Interval);
-
-  appendTens.innerHTML = tens;
-  appendSeconds.innerHTML = seconds;
-});
-
-buttonReset.addEventListener("click", () => {
-  clearInterval(Interval);
-  tens = 00;
-  seconds = 00;
-  appendTens.innerHTML = tens;
-  appendSeconds.innerHTML = seconds;
-});
-
-const startTimer = () => {
-  tens++;
-  sessionStorage.setItem("lastCounterValue", tens);
-  if (tens <= 9) {
-    appendTens.innerHTML = "0" + tens;
-  }
-  if (tens > 9) {
-    appendTens.innerHTML = tens;
-  }
-  if (tens > 99) {
-    console.log("seconds");
-    seconds++;
-    sessionStorage.setItem("lastCounterValue2", seconds);
-    appendSeconds.innerHTML = "0" + seconds;
-    tens = 0;
-    appendTens.innerHTML = "0" + 0;
-  }
-
-  if (seconds > 9) {
-    appendSeconds.innerHTML = seconds;
-  }
+const increaseCounter = () => {
+  setInterval(() => {
+    displayTime.innerHTML = count;
+    sessionStorage.setItem("counterValue", count);
+    count++;
+  }, 1000);
 };
+
+start.onclick = increaseCounter;
